19b5fbef7a9578f9608f19bb9b1b40ad259a1d7e
Chandrakanth Padi <chandrakanth.padi@gmail.com>
2023-02-18 00:39:56 +0530

    added config file

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,6 @@
 module.exports = function (elevtyConfig) {
   elevtyConfig.addPassthroughCopy("./src/styles");
+  elevtyConfig.addPassthroughCopy("./src/admin");
   return {
     dir: {
       input: "src",
